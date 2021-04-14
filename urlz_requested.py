ab8860f83f635cf720e6f81e2033a125cca6d659
Fabian Anguiano <fabiananguiano@gmail.com>
2021-04-13 23:50:54 -0500

    added new version

---
+++ urlz_requested.py
@@ -0,0 +1,58 @@
+import asyncio
+from pprint import pprint
+from pyppeteer import launch
+from pyppeteer.network_manager import NetworkManager
+
+
+url_name = input("Enter a FULL domain to scan: ")
+print("scanning:" + str(url_name))
+
+input_url_list = [url_name]
+
+url_list = input_url_list
+
+refactored_url_list = []
+
+for i in url_list:
+    refactored_url_list.append(i.rstrip("\n"))
+
+for url in refactored_url_list:
+
+    request_listing = []
+
+    def logging(event):
+        req = event._request
+        request_listing.append(req.url)
+
+
+    async def main():
+        browser = await launch({"headless": True})
+        page = await browser.newPage()
+        page._networkManager.on(NetworkManager.Events.Response, logging)
+        await page.goto(url, {'waitUntil': 'networkidle0'})
+
+        await browser.close()
+
+
+    asyncio.get_event_loop().run_until_complete(main())
+
+
+from urllib.parse import urlparse
+
+
+def url_extract(link):
+    return urlparse(link).netloc
+
+
+parsed_urls = []
+
+for url in request_listing:
+    parsed_urls.append(url_extract(url))
+
+
+def pause():
+    programPause = input("Press the <ENTER> key to see list of domains that were parsed")
+    pprint(parsed_urls)
+
+pause()
+
